ec4202549300c6444bd198f884c7a8a9c906ac81
Shubham-Yadav14 <shubhamrao14102002@gmail.com>
2024-08-07 17:03:27 +0530

    port changed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const { verify } = require('./middleware');
 
 const app = express();
-const PORT = process.env.PORT ||5000;
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
